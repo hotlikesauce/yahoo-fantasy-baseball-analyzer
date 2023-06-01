02b960ff8949ff0583ee00dbe46334e49f9cd99c
hotlikesauce <svc_gis@jonahenergy.com>
2023-06-01 11:13:52 -0600

    re-org to loof better on github

--- src/weekly_updates.py
+++ src/weekly_updates.py
@@ -1,7 +1,7 @@
-from get_power_rankings import main as get_power_rankings_main
-from get_all_play import main as get_all_play_main
-from get_season_trend_power_ranks import main as get_season_trend_power_ranks_main
-from get_season_trend_standings import main as get_season_trend_standings_main
+from live_standings.get_power_rankings import main as get_power_rankings_main
+from weekly_stats.get_all_play import main as get_all_play_main
+from weekly_stats.get_season_trend_power_ranks import main as get_season_trend_power_ranks_main
+from weekly_stats.get_season_trend_standings import main as get_season_trend_standings_main
 
 
 # def main():
