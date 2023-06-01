969a1c8977a7736a9a226e334d3ff4b499887582
hotlikesauce <svc_gis@jonahenergy.com>
2023-06-01 16:02:38 -0600

    player_dict_example file

---
+++ src/player_dict_example.py
@@ -0,0 +1,14 @@
+player_dict = {
+"1": 'Name1'
+,"2":'Name2'
+,"3":'Name3'
+,"4":'Name4'
+,"5":'Name5'
+,"6":'Name6'
+,"7":'Name7'
+,"8":'Name8'
+,"9":'Name9'
+,"10":'Name10'
+,"11":'Name11'
+,"12":'Name12'
+}
