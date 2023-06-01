3ff0a9ed4c5c7ddd00d0e3157c1f670d4bd0d13f
hotlikesauce <svc_gis@jonahenergy.com>
2023-06-01 10:10:19 -0600

    hide pyc files

--- weekly_updates.py
+++ weekly_updates.py
@@ -4,19 +4,19 @@ from get_season_trend_power_ranks import main as get_season_trend_power_ranks_ma
 from get_season_trend_standings import main as get_season_trend_standings_main
 
 
-def main():
-    functions = [
-        get_power_rankings_main#,
-        #get_all_play_main,
-        #get_season_trend_power_ranks_main,
-        #get_season_trend_standings_main
-    ]
+# def main():
+#     functions = [
+#         get_power_rankings_main#,
+#         #get_all_play_main,
+#         #get_season_trend_power_ranks_main,
+#         #get_season_trend_standings_main
+#     ]
 
-    for func in functions:
-        try:
-            func()
-        except Exception as e:
-            print(f"Error in {func.__name__}: {str(e)}")
+#     for func in functions:
+#         try:
+#             func()
+#         except Exception as e:
+#             print(f"Error in {func.__name__}: {str(e)}")
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
