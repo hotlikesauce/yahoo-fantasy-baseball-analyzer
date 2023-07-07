765663df984054dbebec1aa380870f9cd87125b5
hotlikesauce <taylorreeseward@gmail.com>
2023-07-06 19:58:59 -0600

    housekeeping

---
+++ src/manager_dict.py
@@ -0,0 +1,14 @@
+manager_dict = {
+"1": 'Taylor'
+,"2":'Austin'
+,"3":'Kurtis'
+,"4":'Bryant'
+,"5":'Greg'
+,"6":'Josh'
+,"7":'Eric'
+,"8":'David'
+,"9":'Jamie'
+,"10":'Kevin'
+,"11":'Mikey'
+,"12":'Cooch'
+}
