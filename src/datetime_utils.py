f4da15ecaf8cfcfa173382511fd0b3711daa2ec3
hotlikesauce <taylorreeseward@gmail.com>
2023-07-14 10:28:13 -0600

    ASG Break Week

--- src/datetime_utils.py
+++ src/datetime_utils.py
@@ -5,7 +5,7 @@ def set_this_week():
     year, week_num, day_of_week = my_date.isocalendar()
     
     # Adjust to match All-Star break. 13 for pre-All-Star.
-    # The if statement below handles the 2-week ASG Break, which happens on week 30 of the calendar year.
+    # The if statement below handles the 2-week ASG Break, which happens on week 29 of the calendar year.
     if week_num < 29:
         return week_num - 13
     else:
@@ -16,7 +16,7 @@ def set_last_week():
     year, week_num, day_of_week = my_date.isocalendar()
     
     # Adjust to match All-Star break. 13 for pre-All-Star.
-    # The if statement below handles the 2-week ASG Break, which happens on week 30 of the calendar year.
+    # The if statement below handles the 2-week ASG Break, which happens on week 29 of the calendar year.
     if week_num < 29:
         return week_num - 14
     else:
