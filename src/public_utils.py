a43e53d44fd1d31eef32d9761de67254e960384b
hotlikesauce <taylorreeseward@gmail.com>
2023-07-14 10:26:19 -0600

    week 29 not 30 for ASG Break

--- src/public_utils.py
+++ src/public_utils.py
@@ -19,8 +19,8 @@ class LiveStandings:
         year, week_num, day_of_week = my_date.isocalendar()
         
         # Adjust to match All-Star break. 13 for pre-All-Star.
-        # The if statement below handles the 2-week ASG Break, which happens on week 30 of the calendar year.
-        if week_num < 30:
+        # The if statement below handles the 2-week ASG Break, which happens on week 29 of the calendar year.
+        if week_num < 29:
             return week_num - 13
         else:
             return week_num - 14
